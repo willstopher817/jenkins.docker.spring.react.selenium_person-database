7859016d138685660bbdd09e51be78869df3e38a
git-leon <xleonhunter@gmail.com>
2020-12-22 18:37:37 -0500

    added teardown to test

--- integration-testing-application/src/test/java/com/github/curriculeon/LandingPageTest.java
+++ integration-testing-application/src/test/java/com/github/curriculeon/LandingPageTest.java
@@ -1,9 +1,11 @@
 package com.github.curriculeon;
 
 import com.git_leon.leonium.DirectoryReference;
+import com.git_leon.leonium.browsertools.browserhandler.BrowserHandler;
 import com.git_leon.leonium.browsertools.browserhandler.BrowserHandlerInterface;
 import com.git_leon.leonium.browsertools.browserhandler.reporting.BrowserHandlerLayeredLogger;
 import com.git_leon.leonium.browsertools.factories.BrowserHandlerFactory;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,21 +20,27 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class LandingPageTest {
     private WebDriver driver;
     private static final String reportName = "Site Traversal Reports";
+    private BrowserHandlerInterface browserHandler;
 
     @Before
     public void setup() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
         this.driver = BrowserHandlerFactory.CHROME.getDriver(capabilities);
-    }
-
-    private void test(String firstName, String lastName) {
-        // given
-        BrowserHandlerInterface browserHandler = new BrowserHandlerLayeredLogger(driver, DirectoryReference
+        this.browserHandler = new BrowserHandlerLayeredLogger(driver, DirectoryReference
                 .TARGET_DIRECTORY
                 .getFileFromDirectory("Report-" + System.nanoTime() + ".html")
                 .getAbsolutePath(),
                 "test-" + Long.toHexString(System.nanoTime()));
+    }
+
+    @After
+    public void teardown() {
+        new BrowserHandler(driver).close();
+    }
+
+    private void test(String firstName, String lastName) {
+        // given
         browserHandler.getOptions().SCREENSHOT_ON_EVENT.setValue(false);
 
         String expectedListOutput = firstName.concat(" ").concat(lastName);
