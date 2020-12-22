9ddc62a0cd339c0687aa47f4665bc29785db4a31
git-leon <xleonhunter@gmail.com>
2020-12-22 17:56:00 -0500

    stubbed base implementation for integration testing's webpage model

---
+++ integration-testing-application/src/main/java/com/github/curriculeon/LandingPage.java
@@ -0,0 +1,58 @@
+package com.github.curriculeon;
+
+import com.git_leon.leonium.browsertools.WebPage;
+import com.git_leon.leonium.browsertools.With;
+import com.github.curriculeon.utils.ApplicationProperties;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by leon on 12/22/2020.
+ */
+public class LandingPage extends WebPage {
+    private ApplicationProperties applicationProperties = new ApplicationProperties();
+    private By byInputFirstName = With.tagAttributeValue("input", "name", "fname");
+    private By byInputLastName = With.tagAttributeValue("input", "name", "lname");
+    private By byButtonSubmit = With.buttonText("Submit");
+    private By byUnorderedListCustomerList = By.cssSelector("#customer-list > ul:nth-child(2) > li");
+
+
+    public LandingPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public void navigateTo() {
+        Integer webApplicationPortNumber = applicationProperties.getConfig().getInt("server-port");
+        getBrowserHandler().navigateTo("localhost:".concat(webApplicationPortNumber.toString()));
+    }
+
+    public void sendInputToFirstName(String input) {
+        getBrowserHandler().sendKeys(byInputFirstName, input);
+    }
+
+    public void sendInputToLastName(String input) {
+        getBrowserHandler().sendKeys(byInputLastName, input);
+    }
+
+    public void clickSubmitButton() {
+        getBrowserHandler().click(byButtonSubmit);
+    }
+
+    public List<String> getCustomerNameList() {
+        List<String> list = new ArrayList<>();
+        getBrowserHandler()
+                .getElements(byUnorderedListCustomerList)
+                .forEach(element -> list.add(element.toString()));
+        return list;
+    }
+
+    public Boolean validateNameHasBeenInput(String nameToSeek) {
+        return getCustomerNameList()
+                .stream()
+                .anyMatch(name -> name.equalsIgnoreCase(nameToSeek));
+    }
+}
